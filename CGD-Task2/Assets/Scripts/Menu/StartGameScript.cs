97f45ebfe338fefb85d1f11e95ac55dfd022f8f9
Tom Southern <tom2.southern@live.uwe.ac.uk>
2019-10-23 19:16:39 +0100

    added skeleton for scene manager & Menu

---
+++ CGD-Task2/Assets/Scripts/Menu/StartGameScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGameScript : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        Debug.Log("START GAME");
+        Loader.Load(Loader.Scene.SampleScene); //TEMP need to change to gameplay scene
+    }
+}

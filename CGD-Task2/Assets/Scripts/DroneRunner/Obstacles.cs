c7bb4e1425d2f3c94d3f64ea5aade0c4a38a310a
Tom <Thomas2.Paradise@live.uwe.ac.uk>
2019-10-28 21:46:02 +0000

    start of objects spawning + moving

---
+++ CGD-Task2/Assets/Scripts/DroneRunner/Obstacles.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacles : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 5.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move downwards and delete if reach the bottom
+        Vector3 pos = gameObject.transform.position;
+
+        pos.y -= speed * Time.deltaTime;
+        gameObject.transform.position = pos;
+
+        if(pos.y <= -6)
+        {
+            Destroy(gameObject);
+        }        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //if collides with player
+        if(collision.gameObject.tag == "player")
+        {
+
+        }
+    }
+}
---
+++ CGD-Task2/Assets/Scripts/DroneRunner/Spawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] spawners;
+    public GameObject[] prefabs;
+    private float max_timer = 1.0f;
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = max_timer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //when timer reaches 0 spawn an instance of obstacle or item at randomised spawner pos;
+        timer -= Time.deltaTime;
+        if(timer <= 0.0f)
+        {
+            timer = max_timer;
+
+            Vector3 position = spawners[Random.Range(0, 3)].transform.position;
+            Instantiate(prefabs[0], position, Quaternion.identity);
+        }
+    }
+}

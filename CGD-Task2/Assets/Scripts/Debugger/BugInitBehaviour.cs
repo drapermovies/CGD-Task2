c0430c1e7814c7ce13b6d0888b2bb13809309a52
Unknown <Zachary2.Horton@live.uwe.ac.uk>
2019-10-30 01:30:49 +0000

    Bugs spawn and move to middle, will need adjustments

---
+++ CGD-Task2/Assets/Scripts/Debugger/BugInitBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BugInitBehaviour : MonoBehaviour
+{
+    public GameObject middle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(middle.transform.position);
+
+        if (Vector3.Distance(transform.position, middle.transform.position) > 2)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, middle.transform.position, Time.deltaTime);
+        }
+        
+    }
+}
---
+++ CGD-Task2/Assets/Scripts/Debugger/BugSpawner.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BugSpawner : MonoBehaviour
+{
+    private Transform[] spawn_points;
+    public GameObject bug_prefab;
+
+    private float timer;
+    private float time_max = 7.0f;
+    private float time_min = 2.0f;
+    private float spawn_time;
+
+    private int spawn_number;
+    private int spawn_min = 0;
+    private int spawn_max = 4;
+    public GameObject[] spawns;
+
+
+    void Start()
+    {
+        spawns = new GameObject[4];
+        setTimer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= spawn_time)
+        {
+            spawnBug();
+            setTimer();
+        }
+    }
+
+    void setTimer()
+    {
+        spawn_time = Random.Range(time_min, time_max);
+        timer = 0.0f;
+    }
+
+    void spawnBug()
+    {
+        spawn_number = Random.Range(spawn_min, spawn_max);
+        
+        switch(spawn_number)
+        {
+            case 0:
+                Instantiate(bug_prefab, transform.position, Quaternion.identity, transform);
+              break;
+
+            case 1:
+                Instantiate(bug_prefab, gameObject.transform.GetChild(0).transform.position, Quaternion.identity, transform);
+                break;
+
+            case 2:
+                Instantiate(bug_prefab, gameObject.transform.GetChild(1).transform.position, Quaternion.identity, transform);
+                break;
+
+            case 3:
+                Instantiate(bug_prefab, gameObject.transform.GetChild(2).transform.position, Quaternion.identity, transform);
+                break;
+        }
+        Debug.Log(gameObject.transform.GetChild(2).name);
+    }
+}

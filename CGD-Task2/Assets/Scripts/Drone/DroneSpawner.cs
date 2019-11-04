0c77f138dea7d82124703e44c3ee6b7ffbcd9a62
D25-Bennett <daniel9.bennett@live.uwe.ac.uk>
2019-11-04 12:14:20 +0000

    Merged Debugger Combined and Drone
    Stopped script naming conflicts

---
+++ CGD-Task2/Assets/Scripts/Drone/DroneSpawner.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneSpawner : MonoBehaviour
+{
+    public GameObject[] spawners;
+    public GameObject[] prefabs;
+
+    private float max_timer = 1.25f;
+    private float timer;
+
+    private int pickup_counter;
+    private int pickup_rate = 10;
+
+    private int last_spawner = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = max_timer;
+        pickup_counter = pickup_rate;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (StressManager.GetBurnout())
+        {
+            max_timer = 0.75f;
+        }
+        else
+        {
+            max_timer = 1.25f;
+        }
+        //when timer reaches 0 spawn an instance of obstacle or item at randomised spawner pos;
+        timer -= Time.deltaTime;
+        if(timer <= 0.0f)
+        {
+            timer = max_timer;
+            last_spawner = chooseSpawner(last_spawner);
+            Vector3 position = spawners[last_spawner].transform.position;
+
+            GameObject spawn;
+            if(pickup_counter == 0)
+            {
+                pickup_counter = pickup_rate;
+                spawn = prefabs[1];
+            }
+            else
+            {
+                spawn = prefabs[0];
+            }
+
+            Instantiate(spawn, position, Quaternion.identity);
+            pickup_counter--;
+        }
+    }
+
+    int chooseSpawner(int previous)
+    {
+        int new_spawner = Random.Range(0, 3);
+
+        //if new spawner is the same as previous
+        //change to one of the other 2
+        if(new_spawner == previous)
+        {
+            if(new_spawner == 2)
+            {
+                new_spawner--;
+            }
+            else if(new_spawner == 0)
+            {
+               new_spawner++;
+            }
+            else
+            {
+            if (Random.Range(0, 2) == 0)
+                {
+                    new_spawner++;
+                }
+                else
+                {
+                    new_spawner--;
+                }
+            }            
+        }
+        return new_spawner;
+    }
+}
---
+++ CGD-Task2/Assets/Scripts/RobotReconstruction/RobotPart.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class RobotPart : MonoBehaviour
+{
+    [SerializeField] private RobotPartsEnum part = RobotPartsEnum.PART_HEAD;
+
+    private bool was_seen = false;
+
+    Vector3 target_scale = Vector3.zero;
+
+    Vector3 mouse_offset = Vector3.zero;
+
+    /*
+     * Called just after object creation
+     */
+    private void Start()
+    {
+        GenerateBodyPart();
+        target_scale = transform.localScale;
+        //transform.localScale = new Vector3(0.1f, 0.1f, 0.1f); //Code to make object appear nice when spawning by scaling up the obj
+    }
+
+    /*
+     * Called on destruction
+     */
+    private void OnDestroy()
+    {
+        FindObjectOfType<Spawner>().DestroyBodyPart(this.transform);
+    }
+
+    /*
+     * Called once a frame
+     */
+    private void Update()
+    {
+        Renderer render = GetComponent<Renderer>();
+
+        //ScaleTransform(); //Was gonna implement code that makes it nicer to look at when objects spawn
+
+        if (render.isVisible && !was_seen)
+        {
+            was_seen = true;
+        }
+        else if (!render.isVisible && was_seen)
+        {
+            if (!Debug.isDebugBuild)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    /*
+     * Assigns a random body part type to the object
+     */
+    void GenerateBodyPart()
+    {
+        int part_num = Random.Range(0, 3);
+
+        part = (RobotPartsEnum)part_num;
+
+        Debug.Log(part);
+    }
+
+    /*
+     * Moves object along conveyor belt
+     */
+    public void ConveyorMovement(float speed, Vector3 dir)
+    {
+        Vector3 position = transform.position;
+        float new_speed = speed * Time.deltaTime;
+
+        position.x += new_speed * dir.x;
+        position.y += new_speed * dir.y;
+
+        transform.position = position;
+    }
+
+    private void OnMouseDown()
+    {
+        mouse_offset = transform.position - GetMouseWorldPos();
+    }
+
+    /*
+     * Called when the mouse hovers over an object
+     */
+    private void OnMouseOver()
+    {
+        GetComponent<SpriteRenderer>().color = Color.red;
+
+        if (Input.GetMouseButton(0))
+        {
+           transform.parent = null; //Removes the spawner as a parent
+        }
+    }
+
+    /*
+     * Called when the mouse is dragged
+     */
+    private void OnMouseDrag()
+    {
+        transform.position = GetMouseWorldPos() - mouse_offset;
+    }
+
+    /*
+     * Called when the mouse is no longer hovering over an object
+     */
+    private void OnMouseExit()
+    {
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+
+    private void ScaleTransform()
+    {
+        if (transform.localScale == target_scale)
+            return;
+
+        Vector3 local_scale = transform.localScale;
+        local_scale.x *= (8 * Time.deltaTime);
+        local_scale.y *= (8 * Time.deltaTime);
+        transform.localScale = local_scale;
+    }
+
+    /*
+     * Converts the mouse pos from screen coordinates to world coordinates
+     */
+    Vector3 GetMouseWorldPos()
+    {
+        Vector3 mouse_pos = Input.mousePosition;
+
+        mouse_pos.z = Camera.main.WorldToScreenPoint(transform.position).z;
+
+        return Camera.main.ScreenToWorldPoint(mouse_pos);
+    }
+}
